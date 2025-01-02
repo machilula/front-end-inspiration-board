dbbf85fd51e12dd0fbf931570d020d56dd6e5d83
Charday Neal <charday.neal@gmail.com>
2025-01-02 11:14:28 -0800

    Implement hide and show board form

--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -2,18 +2,26 @@ import { useState } from 'react';
 
 const initialValue = {
   title: '',
-  creator: '',
+  creator: ''
 };
 
 export const NewBoardForm = (props) => {
   const [board, setBoard] = useState(initialValue);
+  const [showBoard, setShowBoard] = useState(true);
+  
   
   console.log(board); 
+  
 
   const handleBoardChange = (event) => {
     setBoard(prevBoard => {
       return { ...prevBoard, [event.target.id]: event.target.value}
     });
+  };
+
+  const handleShowForm = () => {
+    setShowBoard(true);
+    setBoard(initialValue);
   };
 
   const handleSubmit = (event) => {
@@ -22,25 +30,36 @@ export const NewBoardForm = (props) => {
     setBoard(initialValue);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
+  if (showBoard) {
+    return (
       <div>
-        <label htmlFor='title'>Title</label>
-        <input 
-          type='text' 
-          id='title' 
-          value={board.title} 
-          onChange={handleBoardChange}/>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='title'>Title</label>
+            <input 
+              type='text' 
+              id='title' 
+              value={board.title} 
+              onChange={handleBoardChange}/>
+          </div>
+          <div>
+            <label htmlFor='creator'>Creator</label>
+            <input 
+              type='text' 
+              id='creator' 
+              value={board.creator} 
+              onChange={handleBoardChange}/>
+          </div>
+          <input type="submit" value='Create'/>
+        </form>
+        <button onClick={() => setShowBoard(false)}>Hide New Board</button>
       </div>
-      <div>
-        <label htmlFor='creator'>Creator</label>
-        <input 
-          type='text' 
-          id='creator' 
-          value={board.creator} 
-          onChange={handleBoardChange}/>
-      </div>
-      <input type="submit" value='Create'/>
-    </form>
-  );
+    )
+  } else {
+    return (
+      <button onClick={handleShowForm}>Show New Board</button>
+    );
+  }
+
+  
 };
