ac3b4108ba117c01dd83620addabd6142cc779a5
Lula <mlusan95@gmail.com>
2025-01-03 13:23:07 -0500

    made changes to board component and imported card and cardlistto board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,3 +1,18 @@
+import { CardList } from "./CardList";
+import { NewCardForm } from "./NewCardForm";
+
 export const Board = (props) => {
-  return <option value={props.title}>{props.title}</option>;
+  return (
+    <div>
+      <section>
+        <h2>Cards for {props.title}</h2>
+        <CardList cards={props.cards}/>
+      </section>
+      <section>
+        <NewCardForm onNewCard={props.onNewCard}/>
+      </section>
+    </div>
+
+  )
+
 };
--- src/components/BoardsList.jsx
+++ src/components/BoardsList.jsx
@@ -2,14 +2,14 @@ import { Board } from './Board';
 
 export const BoardsList = (props) => {
   const allBoards = props.boards.map((board, index) => {
-    return <Board key={index} title={board.title}/>
+    return <option key={index} value={board.title}>{board.title}</option>
   });
 
   return (
     <div>
       <h2>Boards</h2>
       <select onChange={() => console.log('You\ve Changed!')}>
-       {allBoards}
+      {allBoards}
       </select>
     </div>
   );
