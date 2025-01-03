3c29c355d3db7af483745dbab1938a23be58c0dc
Lula <mlusan95@gmail.com>
2025-01-03 16:18:46 -0500

    delete wip, added a show/hide button for card form, changed cardlist

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,6 +1,11 @@
 import { Card } from './Card';
 
 export const CardList = (props) => {
+
+  const handleDeleteCard = (id) => {
+    
+  };
+
   const allCards = props.cards.map((card, index) => {
       return <Card key={index} title={card.message}/>
   });
