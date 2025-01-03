46267a12fb651a3745ad5e9169f1665c40229131
Lula <mlusan95@gmail.com>
2025-01-03 16:29:22 -0500

    resolving merge conflicts with card components

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export const Card = (props) => {
+  const [likeCount, setLikeCount] = useState(0);
+
+  const onLikeClick = () => {
+    setLikeCount(likeCount + 1);
+  };
+
+
+  return (
+    <li>
+      <h3>{props.message}</h3>
+      <h3>{props.likes}</h3>
+      <button onClick={onLikeClick}>💜</button>
+    </li>
+  )
+};
