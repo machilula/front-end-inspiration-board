8873982837858f9d491a51ec68cd1a4a08dfbc24
Lula <mlusan95@gmail.com>
2025-01-07 13:13:30 -0500

    comments made for requests

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,9 @@ function App() {
   const [selectedBoard, setSelectedBoard] = useState({});
   const [isBoardSelected, setIsBoardSelected] = useState(false);
 
+  // use useEffect()
+    // make a GET call to API to retrieve the 
+
   const addBoard = (newBoard) => {
     newBoard['cards'] = [];
     setBoardData(prevBoard => [...prevBoard, newBoard]);
@@ -83,29 +86,35 @@ function App() {
 
   //  addCard does not render a new card. WIP
   const addCard = (boardId, newCard) => {
+    // make a post request to add new card
+
     // call setselectedboard to copy board object
     // pass in newcards to new board object
     // call setboarddata to update the board object
 
     // let newCards = [];
-    setSelectedBoard((prevBoard) => {
-      return {...prevBoard, cards: prevBoard.cards.push(newCard)};
-    });
+    // setSelectedBoard((prevBoard) => {
+    //   return {...prevBoard, cards: prevBoard.cards.push(newCard)};
+    // });
 
     // setBoardData((prevBoard) => {
     //   console.log([...prevBoard, selectedBoard])
     //   return [...prevBoard, selectedBoard];
     // })
-
-
-    setBoardData(boardData => boardData.map(board => {
+    setBoardData((boardData) => boardData.map(board => {
       if (board.id === boardId) {
-        // newCards = [...board.cards];
-        // newCards.push(newCard);
-        board['cards'] = [board.cards, newCard]
-      }
+        return {...board, cards: [...board.cards, newCard]};
+      } else return board;
+      // newCards = [...board.cards];
+      // newCards.push(newCard);
+      // board['cards'] = [board.cards, newCard]
+      
     }));
+    
+    selectBoard(boardId);
   };
+  
+
 //  deleteCard event handler WIP, does not work
   const deleteCard = (boardId, cardId) => {
     setBoardData(boardData => boardData.map(board => {
@@ -126,6 +135,7 @@ function App() {
       </header>
       <main>
         <section className='boards-container'>
+          {/* do the functions being passed down to board  */}
           <BoardsList boards={boardData} onSelectBoard={selectBoard} />
           <div>
             <h2> Selected Board </h2>
@@ -134,6 +144,7 @@ function App() {
           <NewBoardForm onNewBoard={addBoard}/>
         </section>
         <section>
+          {/* when Board passes down the funcs, how does it update boardData? */}
           {isBoardSelected ? <Board board={selectedBoard} onNewCard={addCard} onDeleteCard={deleteCard}/> : ''}
         </section>
       </main>
